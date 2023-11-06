0db267a7a646f7e4563d715925f9ece7b83406cd
Shashi Pandey <kr.shashi123pandey@gmail.com>
2023-11-06 12:38:21 +0530

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 const App = () => {
-  return (
-    <h6 className=" text-3xl font-bold underline">
-      Hello world!
-    </h6>
-  )
-};
-
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    <Route path="/Sign-in" element={<SignIn/>}/>
+    <Route path="/Sign-up" element={<SignUp/>}/>
+  </Routes>
+  </BrowserRouter>;
+}
 export default App;
