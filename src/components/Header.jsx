a1335655a3f7529d2b92bc6ba97fc7e9135b7c56
Shashi Pandey <kr.shashi123pandey@gmail.com>
2023-11-17 23:41:44 +0530

    2 commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
         <header className='bg-sky-200 shadow-md '>
             <div className=' flex justify-between items-center max-w-6xl mx-auto p-3 '>
                 <Link to="/">
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap '>
             <span className='text-slate-500 '>MERN</span>
             <span className='text-slate-700 '>Estate</span>
             </h1>
